6a3b0fdb660ec74360490942724cc1e40db2ec42
Liam Woods <143724842+Liam-Woods-8@users.noreply.github.com>
2025-03-17 00:21:29 -0400

    Fix gradient color classes for Tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,10 +15,16 @@ const config: Config = {
         'forest': '#99BC85',
         'text': '#2C3E2D',
       },
-    },
-    fontFamily: {
-      'sao-torpes': ['Sao Torpes', 'sans-serif'],
-      'ramona': ['Ramona Light', 'sans-serif'],
+      gradientColorStops: {
+        'forest': '#99BC85',
+        'sage': '#E4EFE7',
+        'cream': '#FAF1E6',
+      },
+      fontFamily: {
+        'sao-torpes': ['Sao Torpes', 'sans-serif'],
+        'ramona': ['Ramona', 'sans-serif'],
+        'ramona-light': ['Ramona Light', 'sans-serif'],
+      },
     },
   },
   plugins: [],
